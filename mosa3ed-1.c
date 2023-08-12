20250542f688d337db27c0153a122677622f175e
Sawsanhenedy <sawsanhenedy92@gmail.com>
2023-08-12 11:05:56 -0700

    c

--- mosa3ed-1.c
+++ mosa3ed-1.c
@@ -9,34 +9,6 @@ int _putchar(char a)
 return (write(1, &a, 1));
 }
 /**
- * str_drk - druckt einen Buchstabe
- * @b: str
- * Return: nummer des char im str
- */
-int str_drk(char *b)
-{
-int a, s = 0;
-if (b != NULL)
-{
-for (a = 0; b[a] != '\0'; a++)
-{
-_putchar(b[a]);
-}
-return (a);
-}
-else
-{
-_putchar('(');
-_putchar('n');
-_putchar('u');
-_putchar('l');
-_putchar('l');
-_putchar(')');
-s = 6;
-return (s);
-}
-}
-/**
  * _sc - kopiert ein str
  * @r: masdar
  * @d: pointer
@@ -67,26 +39,4 @@ l++;
 j++;
 }
 return (l);
-}
-/**
- * _sd - str x2
- * @a: str
- * Return: NULL -> Null, pointer -> Succes
- */
-char *_sd (char *a)
-{
-int j = 0, l = 0;
-char *ns;
-if (a == NULL)
-return (NULL);
-while (a[j] != '\0')
-{
-l++;
-j++;
-}
-ns = malloc(sizeof(char) * (l + 1));
-for (j = 0; j < a[j]; j++)
-ns[j] = a[j];
-ns[j] = '\0';
-return (ns);
 }
