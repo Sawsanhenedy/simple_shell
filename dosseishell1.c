2d00d59614abf22d7ce56a7c4a97e8d2a8fae58f
salma <salmahelaltur@gmail.com>
2023-08-12 18:14:00 -0700

    1

---
+++ dosseishell1.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdint.h>
+
+/*
+ * wazefa_1 - func1
+ * @mod5_1: vp1
+ * @mod5_2: vp2
+ * @mod5_3: vp3
+ * @mod5_4: vp4
+ * Return: rakam ma
+ */
+
+int wazefa_1( char **mod5_1, pid_t mod5_2, char **mod5_3, int mod5_4)
+{
+	char *mot8yer = mod5_3[0];
+	char *mot8_2 = NULL;
+	int *mot8_3;
+
+	
