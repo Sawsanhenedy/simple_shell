b97a3b68fab6c97e2a391b61b974caf9bf799e82
salma <salmahelaltur@gmail.com>
2023-08-14 07:42:08 -0700

    2

--- main.h
+++ main.h
@@ -10,9 +10,47 @@
 #include<sys/wait.h>
 #include<signal.h>
 #include<stddef.h>
-volatile int ctrlc_clean;
-extern char **environ;
 
+volatile int v_m2; //ctrlc_clean
+extern char **vm1; //**environ
+
+/* struct & typedef */
+
+/**
+ * struct bultin - struct for bultin cmd
+ * @name: name of cmd
+ * @handler: handler for the cmd
+ */
+
+typedef struct bultin
+{
+	char *name;
+	void (*handler)(char **, char **);
+} bultin;
+
+typedef void (*sighandler_t)(int);
+
+
+/* Funktionen von  */
+
+// 1
+int wazefa_1( char **mod5_1, pid_t mod5_2, char **mod5_3
+		, int mod5_4); //exec_fullcmd
+int wazefa_2(char **mod5_1, char *mod5_2, int mod5_3
+		, char ***mod5_4,char **mod5_5); //shellbultin
+char *wazwfa_3(char **mod5_1, char *mod5_2); //*_getenv
+// 2
+void wazefa_1(int mod5_1); //ctrlc_handler
+void wazefa_2(char *mod5_1); //err_notfoundvoid wazefa_3(char **mod5_1, char ***mod5_2, char **mod5_3); //_handleenv
+int wazefa_4(char **mod5_1, pid_t mod5_2, int *mod5_3); //exec_cmd
+void wazefa_5(char **mod5_1); //_printenv
+// 3
+int wazefa_1(char **mod5_1); //_isvalidenvcmd
+int wazefa_2(int mod5_1, int mod5_2, char **mod5_3); //exec_returnhandler
+void wazefa_3(int mod5_1, char **mod5_2, char *mod5_3,
+		char **mod5_4, char *mod5_5); //exitshell
+void wazefa_4(char *mod5_1, char *mod5_2, int mod5_3,
+		char ***mod5_4, char **mod5_5); //_addenv
 /* funktionen von Mosa3ed */
 char *_sc(char *d, char *r);
 int _putchar(char a);
@@ -21,8 +59,4 @@ char *_sd(char *a);
 int _sncm(char *a1, char *a2, int m);
 int str_drk(char *b);
 int _atoi(char *a);
-
-/* Funktionen von .... */
-
-
 #endif
