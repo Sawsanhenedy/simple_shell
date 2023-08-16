c00dc6bd8fa55e42f2645ba3576fe42f893e264a
Sawsanhenedy <sawsanhenedy92@gmail.com>
2023-08-16 09:18:41 -0700

    m

---
+++ shell.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * main - bildt unserer SHELL
+ * @n: Argumenten nummer
+ * @r: Argumenten ar
+ * @v: env
+ * Return: Always Success
+ */
+int main(int __attribute__((unused)) n,
+__attribute__((unused)) char **r,
+__attribute__((unused)) char **v)
+{
+pid_t p = 0;
+ssize_t g_s;
+char *c = NULL, *ne = NULL;
+size_t s = 50;
+const char *d = " \t\f\r\n";
+int c = 0, cs = 0, er, tr;
+char **a = NULL, **nev = NULL;
+v_m2 = 1;
+while (v_m2)
+{
+
