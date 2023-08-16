9ec51cc372348360751328a6ac1f25cb40d5219f
Sawsanhenedy <sawsanhenedy92@gmail.com>
2023-08-16 06:34:34 -0700

    f

--- main.h
+++ main.h
@@ -35,22 +35,22 @@ typedef void (*sighandler_t)(int);
 
 // 1
 int wazefa_1( char **mod5_1, pid_t mod5_2, char **mod5_3
-		, int mod5_4); //exec_fullcmd
+		, int mod5_4); // exec_fullcmd 
 int wazefa_2(char **mod5_1, char *mod5_2, int mod5_3
-		, char ***mod5_4,char **mod5_5); //shellbultin
-char *wazwfa_3(char **mod5_1, char *mod5_2); //*_getenv
-// 2
-void wazefa_1(int mod5_1); //ctrlc_handler
+		, char ***mod5_4,char **mod5_5); // shellbultin 
+char *wazwfa_3(char **mod5_1, char *mod5_2); // *_getenv
+// 2 
+void wazefa_1(int mod5_1); // ctrlc_handler 
 void wazefa_2(char *mod5_1); //err_notfoundvoid wazefa_3(char **mod5_1, char ***mod5_2, char **mod5_3); //_handleenv
-int wazefa_4(char **mod5_1, pid_t mod5_2, int *mod5_3); //exec_cmd
-void wazefa_5(char **mod5_1); //_printenv
+int wazefa_4(char **mod5_1, pid_t mod5_2, int *mzod5_3); // exec_cmd
+void wazefa_5(char **mod5_1); // _printenv
 // 3
-int wazefa_1(char **mod5_1); //_isvalidenvcmd
-int wazefa_2(int mod5_1, int mod5_2, char **mod5_3); //exec_returnhandler
+int wazefa_1(char **mod5_1); // isvalidenvcmd
+int wazefa_2(int mod5_1, int mod5_2, char **mod5_3); // exec_returnhandler 
 void wazefa_3(int mod5_1, char **mod5_2, char *mod5_3,
-		char **mod5_4, char *mod5_5); //exitshell
+		char **mod5_4, char *mod5_5); // exitshell
 void wazefa_4(char *mod5_1, char *mod5_2, int mod5_3,
-		char ***mod5_4, char **mod5_5); //_addenv
+		char ***mod5_4, char **mod5_5); // _addenv
 /* funktionen von Mosa3ed */
 char *_sc(char *d, char *r);
 int _putchar(char a);
